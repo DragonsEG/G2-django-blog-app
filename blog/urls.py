318129823dbfd4b801c5c2778e9f25fcf4b6a0f0
Eslam Sayed <esla889900@gmail.com>
2023-09-27 23:58:32 +0300

    final edition

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,13 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('', views.Index.as_view(), name='index'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', views.loginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('create/', views.createView.as_view(), name="Create"),
     path('blog/<int:pk>/', views.DetailPostView.as_view(), name='detail_post'),
-
+    path('profile/<str:user>/' , views.profileView.as_view() , name = 'profile') ,
+    path('delAccount/<str:user>' , views.deleteAccount , name = "deleteAccount") , 
+    path('changepassword' , views.changePass.as_view(), name = 'changepassword')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegisterForm
+from .forms import UserLoginForm , ChangeForm
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView 
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth import logout , login , authenticate
 from django.contrib import messages
 from .models import Post, Comment
 from django.contrib.auth.models import User,Group
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth import update_session_auth_hash
 # Create your views here.
 
 
@@ -31,7 +36,7 @@ class RegisterView(View):
                 group = get_object_or_404(Group, name="Admin-Group")
                 user.groups.add(group)
                 user.save()
-            elif form.cleaned_data['is_staff'] == True:
+            elif form.cleaned_data['is_staff'] == True: 
                 group = get_object_or_404(Group,name='Editor-Group')
                 user.groups.add(group)
                 user.save()
@@ -41,12 +46,39 @@ class RegisterView(View):
                 user.save()
             messages.success(
                 request, "Your are signed up successfully")
-            return redirect('index')
+            user = authenticate(request, username=f"{form['username'].value()}", password=f"{form['password1'].value()}")
+            login(request , user)
+            return redirect('profile' ,form['username'].value())
         else:
             messages.error(
                 request, "Wrong input data please re enter it.")
             return redirect('register')
 
+class loginView (View) :
+    def get(self, request):
+        form = UserLoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    def post(self, request):
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            if (User.objects.all().filter(username=form['username'].value()).exists()) :
+                if check_password(form['password'].value() ,User.objects.all().filter(username=form['username'].value())[0].password ) :
+                    user = authenticate(request, username=f"{form['username'].value()}", password=f"{form['password'].value()}")
+                    login(request , user)
+                    return redirect('profile' ,form['username'].value())
+                else :
+                    messages.error(
+                    request, "Wrong password.")
+                    return redirect('login')
+            else :
+                messages.error(
+                    request, "Not exist.")
+                return redirect('login')
+        else:
+            messages.error(
+                request, "Wrong input data please re enter it.")
+            return redirect('login')
 
 class createView (View):
     def get(self, request):
@@ -80,3 +112,51 @@ class DetailPostView(DetailView):
         Comment.objects.create(
             post_id=pk, comment_content=request.POST['comment_field'], user_id=request.user.id)
         return redirect('detail_post',pk)
+    
+def deleteAccount (request , user) :
+    if request.method == 'GET' :
+        print(user)
+        logout(request)
+        get_object_or_404(User , username = user).delete() 
+        return redirect('index')
+    else :
+        return HttpResponse('Bad request')
+
+class changePass (PasswordChangeView) :
+    def get(self, request):
+        form = ChangeForm
+        return render(request, 'blog/change password.html', {'form': form})
+    def post (self , request) :
+        form = ChangeForm(request.POST) 
+        if (form.is_valid()) :
+            if check_password(form['old_password'].value() , request.user.password) :
+                if (form['new_password1'].value() == form['new_password2'].value()):
+                    test = User.objects.all().filter(username = request.user.username)
+                    test[0].password = form['old_password'].value() 
+                    test[0].save() 
+                    new_user = User.objects.all().filter(username = request.user.username)
+                    update_session_auth_hash(request , new_user)
+                    messages.success(
+                        request, "Your password changed")
+                    return redirect('profile' , request.user)
+                else :
+                    messages.error(
+                    request, "Wrong input data")
+                    return redirect('changepassword')
+            else :
+                messages.error(
+                request, "Wrong input data")
+                return redirect('changepassword')
+        else :
+            messages.error(
+                request, "Wrong input data")
+            return redirect('changepassword')
+class deletePost (View) :
+    pass
+
+class editPost (View) :
+    pass
+class profileView (View) :
+    def get (self , request , user) :
+        posts = Post.objects.all().filter(owner_id = request.user.id)
+        return render(request , 'users/profile.html' , {'user' : request.user , 'posts' : posts })
