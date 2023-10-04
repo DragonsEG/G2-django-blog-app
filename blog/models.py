2bc0f080d2f032261f6504e7b170960c3643696b
Eslam Sayed <esla889900@gmail.com>
2023-10-04 22:21:25 +0300

    task2 finished

--- blog/models.py
+++ blog/models.py
@@ -4,20 +4,21 @@ from tinymce.models import HTMLField
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
-class Post (models.Model):
+class categories (models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    checked = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
+
+class Post (models.Model):
     class Status(models.TextChoices):
-        DRAFT = 'DF' , 'Draft' 
+        DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
-    class Category(models.TextChoices):
-        NEWS = 'News','News'
-        TUTORIAL = 'Tutorial','Tutorial'
-        BUSINESS = 'Business','Business'
-
     title = models.CharField(max_length=255)
-    statue = models.CharField(max_length=2,choices=Status.choices)
-    categories = models.CharField(max_length=10,choices=Category.choices)
+    statue = models.CharField(max_length=2, choices=Status.choices)
     content = HTMLField()
     owner = models.ForeignKey(
         User,
@@ -25,10 +26,10 @@ class Post (models.Model):
         related_name='posts_owner'
     )
     publish_date = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField('categories')
 
     def __str__(self):
         return self.title
-    
 
 
 class Comment (models.Model):
@@ -47,18 +48,27 @@ class Comment (models.Model):
     def __str__(self):
         return self.comment_content
 
-class company (models.Model) :
-    name = models.CharField(max_length=255) 
+
+class company (models.Model):
+    name = models.CharField(max_length=255)
     mail = models.EmailField()
 
-class CompanyWriters (models.Model) :
+    def __str__(self):
+        return self.name
+
+
+class CompanyWriters (models.Model):
     company = models.ForeignKey(
-        company , 
-        on_delete= models.CASCADE 
+        company,
+        on_delete=models.CASCADE
     )
     Writer = models.ForeignKey(
-        User , 
-        on_delete=models.CASCADE 
+        User,
+        on_delete=models.CASCADE,
+        unique=True
     )
+
+    def __str__(self):
+        return f'{self.company.name} --> {self.Writer.username}'
 
 # Create your models here.
